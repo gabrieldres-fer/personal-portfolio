be2fa48774a044606339ea522cc8d68cceb607ab
gabrieldres-fer <gabrieldres.20@gmail.com>
2023-02-12 10:05:18 -0400

    Botones de navegacion adaptados a moviles

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,46 +1,46 @@
 interface Routes {
-   name: string;
-   scroll: number;
+    name: string;
+    scroll: number;
 }
 
 export const routes: Routes[] = [
-   {
-      name: 'Inicio',
-      scroll: 0,
-   },
-   {
-      name: '¿Quien Soy?',
-      scroll: 500,
-   },
-   {
-      name: 'Portafolio',
-      scroll: 1130,
-   },
-   {
-      name: 'Contactame',
-      scroll: 2300,
-   },
+    {
+        name: 'Inicio',
+        scroll: 0,
+    },
+    {
+        name: '¿Quien Soy?',
+        scroll: 500,
+    },
+    {
+        name: 'Portafolio',
+        scroll: 1130,
+    },
+    {
+        name: 'Contactame',
+        scroll: 2300,
+    },
 ];
 
 const navButtonsContainer = document.getElementById('navButtonsContainer');
 
 routes.forEach((route) => {
-   const btn = document.createElement('button');
-   btn.classList.add(
-      'mr-8',
-      'cursor-pointer',
-      'transition',
-      'ease-in',
-      'duration-200',
-      'hover:text-[#35fd7e]'
-   );
-   btn.innerText = route.name;
-   navButtonsContainer.appendChild(btn);
+    const btn = document.createElement('button');
+    btn.classList.add(
+        'md:mr-8',
+        'cursor-pointer',
+        'transition',
+        'ease-in',
+        'duration-200',
+        'hover:text-[#35fd7e]'
+    );
+    btn.innerText = route.name;
+    navButtonsContainer.appendChild(btn);
 
-   btn.addEventListener('click', () => {
-      window.scroll({
-         top: route.scroll,
-         behavior: 'smooth',
-      });
-   });
+    btn.addEventListener('click', () => {
+        window.scroll({
+            top: route.scroll,
+            behavior: 'smooth',
+        });
+    });
 });
