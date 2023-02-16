9717cbe95a05dfea04a09013657c4a579324b59b
gabrieldres-fer <gabrieldres.20@gmail.com>
2023-02-16 19:25:26 -0400

    Manejando la data de projectos en un objeto

---
+++ src/data/Projects.ts
@@ -0,0 +1,20 @@
+interface Project {
+    title: string;
+    url: string;
+    // Img: string;
+}
+
+export const projects: Project[] = [
+    {
+        title: 'Similique facilis quisquambeatae',
+        url: '#',
+    },
+    {
+        title: 'Similique facilis quisquambeatae',
+        url: '#',
+    },
+    {
+        title: 'Similique facilis quisquambeatae',
+        url: '#',
+    },
+];
