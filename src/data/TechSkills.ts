67c9fe2152aa675b1899de281b2ed34c9d4747e9
gabrieldres-fer <gabrieldres.20@gmail.com>
2023-02-16 20:17:44 -0400

    Manejando las tech skills desde un objeto

---
+++ src/data/TechSkills.ts
@@ -0,0 +1,48 @@
+interface Tech {
+    name: string;
+    classes: string;
+}
+
+export const technologies: Tech[] = [
+    {
+        name: 'Typescript',
+        classes:
+            'text-[#00A3FF] [text-shadow:0_0_25px_#00A3FF] hover:[text-shadow:0_0_30px_#00A3FF] styles-tech',
+    },
+    {
+        name: 'Wordpress',
+        classes:
+            'text-gray-300 [text-shadow:_0_0_25px_#BD00FF] hover:[text-shadow:_0_0_30px_#BD00FF] styles-tech',
+    },
+    {
+        name: 'React',
+        classes:
+            'text-[#00ffff] [text-shadow:_0_0_25px_#00ffff] hover:[text-shadow:_0_0_30px_#00ffff] styles-tech',
+    },
+    {
+        name: 'Astro',
+        classes:
+            'text-[#FF7A00] [text-shadow:_0_0_25px_#FF7A00] hover:[text-shadow:_0_0_30px_#FF7A00] styles-tech',
+    },
+    {
+        name: 'Tailwind',
+        classes:
+            'text-[#00FF94] [text-shadow:_0_0_30px_#00FF94] hover:[text-shadow:_0_0_35px_#00FF94] styles-tech',
+    },
+
+    {
+        name: 'Next.js',
+        classes:
+            'text-[#FF007A] [text-shadow:_0_0_25px_#FF007A] hover:[text-shadow:_0_0_30px_#FF007A] styles-tech styles-tech',
+    },
+    {
+        name: 'Node.js',
+        classes:
+            'text-[#52FF00] [text-shadow:_0_0_25px_#52FF00] hover:[text-shadow:_0_0_30px_#52FF00] styles-tech',
+    },
+    {
+        name: 'Javascript',
+        classes:
+            'text-[#F7DF1E] [text-shadow:0_0_25px_#FFF500] hover:[text-shadow:_0_0_30px_#F7DF1E] styles-tech',
+    },
+];
