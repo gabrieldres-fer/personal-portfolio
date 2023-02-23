a2663313fff97db3bdddb3e76b5d9cfdcd46143d
Gabriel Fernandez <gabrieldres.20@gmail.com>
2023-02-23 18:56:08 -0400

    Aniamacion blur creada

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-   theme: {
-      extend: {
-         colors: {
-            blue: {
-               950: '#04030B',
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                blue: {
+                    950: '#04030B',
+                },
+                teal: {
+                    550: '#00e0bb',
+                },
             },
-            teal: {
-               550: '#00e0bb',
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
             },
-         },
-         fontFamily: {
-            inter: ['Inter', 'sans-serif'],
-         },
-      },
-   },
-   plugins: [],
+            animation: {
+                'shiny-ball': 'shiny-ball 3s linear infinite',
+            },
+            keyframes: {
+                'shiny-ball': {
+                    '0%': { transform: 'rotate(0deg); scale(1)', opacity: '0.7' },
+                    '50%': { opacity: '0.4', transform: 'scale(1.2)' },
+                    '100%': { transform: 'rotate(360deg); scale(1)', opacity: '0.7' },
+                },
+            },
+        },
+    },
+    plugins: [],
 };
